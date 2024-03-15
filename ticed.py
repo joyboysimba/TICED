dadef080ddae4b60087550e38aedd8b54add74b8
joyboysimba <144546854+joyboysimba@users.noreply.github.com>
2024-03-15 14:12:07 +0800

    Update ticed.py

--- ticed.py
+++ ticed.py
@@ -94,45 +94,42 @@ class TICED(nn.Module):
     
     def forward(self, seq, lengths):
         # get original item embedding
-        embs_origin = self.emb(seq)  # seq:(19,bs)
+        embs_origin = self.emb(seq) 
         lengths_ori = lengths
-        embs_origin = embs_origin.permute(1, 0, 2) # (bs,19,es)
+        embs_origin = embs_origin.permute(1, 0, 2) 
 
-        # get position embedding  pos+length
-        item_position = torch.tensor(range(1, seq.size()[0]+1), device=self.device) # [19]
-        item_position = item_position.unsqueeze(1).expand_as(seq).permute(1, 0) # (bs,19)
-        len_d = torch.Tensor(lengths_ori).unsqueeze(1).expand_as(seq.permute(1, 0))*5-1 # (bs,19) 长度的权重
+        # get position embedding 
+        item_position = torch.tensor(range(1, seq.size()[0]+1), device=self.device) 
+        item_position = item_position.unsqueeze(1).expand_as(seq).permute(1, 0) 
+        len_d = torch.Tensor(lengths_ori).unsqueeze(1).expand_as(seq.permute(1, 0))*5 - 1 
         len_d = len_d.type(torch.LongTensor).cuda()
         mask_position = torch.where(seq.permute(1, 0) > 0, torch.tensor([1], device=self.device),
-                           torch.tensor([0], device=self.device)) # (bs,19)
+                           torch.tensor([0], device=self.device))
         
         item_imp = torch.zeros_like(seq)
         for i in range(seq.shape[1]):
-            unique_values, counts = torch.unique(seq[:, i], return_counts=True)
+            unique_values, counts = torch.unique(seq[:, i], return_counts = True)
             item_imp[:, i] = counts[torch.searchsorted(unique_values, seq[:, i])]
 
-        #item_imp = self.emb1(item_imp * mask_position.permute(1, 0)) #(19,bs,64)
-        #item_position = item_position * mask_position * len_d     # (bs,19)
         item_position = item_imp.permute(1, 0) * mask_position 
         
         mask = torch.where(seq.permute(1, 0) > 0, torch.tensor([1.], device=self.device),
-                           torch.tensor([0.], device=self.device)) # 浮点mask (bs,19)
+                           torch.tensor([0.], device=self.device)) 
 
-        hidden = self.init_hidden(seq.size(1)) #(bs,hiddensize)
-        #embs_origin(19,256,64),19是规定的最大长度，length表示每一个会话的真实长度<19
+        hidden = self.init_hidden(seq.size(1)) 
         embs_padded = pack_padded_sequence(embs_origin.permute(1, 0, 2) , lengths) 
         gru_out, hidden = self.gru(embs_padded, hidden)
-        gru_out, lengths = pad_packed_sequence(gru_out) # get user embeding gru_out (real_lenth,bs,hiddensize)
-        now_emb = hidden[-1] # 当前长度的时间信息(bs,hiddensize) 
-        glo_emb = torch.sum(gru_out, 0) #全局信息 real_lenth,bs,hiddensize)->(bs,hiddensize)
+        gru_out, lengths = pad_packed_sequence(gru_out) 
+        now_emb = hidden[-1]
+        glo_emb = torch.sum(gru_out, 0) 
 
-        pos_embs = self.position_dropout(self.position_emb(item_position)) #torch.Size([bs, 19, posdim])
-        pos_embs = self.dropout30(self.pos2item_dim(pos_embs)) # torch.Size([bs, 19, 64])
+        pos_embs = self.position_dropout(self.position_emb(item_position)) 
+        pos_embs = self.dropout30(self.pos2item_dim(pos_embs)) 
         usernow_emb = self.dropout30(self.user2item_dim(now_emb)).unsqueeze(1).expand_as(embs_origin) 
         user_emb_expand = self.dropout30(self.user2item_dim(glo_emb)).unsqueeze(1).expand_as(embs_origin)
 
         user_p = torch.sigmoid(self.w_u_p(user_emb_expand)+self.u_u_p(usernow_emb))
-        user_r = torch.sigmoid(self.w_u_r(user_emb_expand) + self.u_u_r(embs_origin))  #torch.Size([bs, 19, 64])
+        user_r = torch.sigmoid(self.w_u_r(user_emb_expand) + self.u_u_r(embs_origin))  
         uw_emb_h = self.tanh(self.w_u(user_emb_expand) + self.u_u(user_p*embs_origin))
         uw_emb = user_p * user_emb_expand + user_r * uw_emb_h
 
@@ -143,16 +140,16 @@ class TICED(nn.Module):
         embs_final = fre_h*user_emb_expand + fre_e*pw_emb_h
 
         # batchnormalization
-        embs_final = self.bn(embs_final) #torch.Size([bs, 19, 64])
+        embs_final = self.bn(embs_final) 
 
         ini_emb = self.final2std_cur(embs_final.contiguous().view(-1, self.embedding_dim)).view(embs_final.size())
         ini_atten = self.v1_w(embs_final.contiguous().view(-1, self.embedding_dim)).view(embs_final.size())
-        ini_atten = self.tanh(ini_atten)  #torch.Size([bs, 19, 64])
-        ini_w = self.v1(ini_atten).expand_as(ini_emb) * ini_emb  #torch.Size([bs, 19, 64])
+        ini_atten = self.tanh(ini_atten)  
+        ini_w = self.v1(ini_atten).expand_as(ini_emb) * ini_emb 
 
         
-        layer1mask = mask.unsqueeze(2).expand_as(ini_emb) * ini_w   #torch.Size([bs, 19, 64])
-        layer1mask = torch.sum(layer1mask, 1)  #torch.Size([bs, 64])
+        layer1mask = mask.unsqueeze(2).expand_as(ini_emb) * ini_w   
+        layer1mask = torch.sum(layer1mask, 1)
         layer1mask = mask.unsqueeze(2).expand_as(ini_emb) * layer1mask.unsqueeze(1).expand_as(ini_emb)
 
 
@@ -160,34 +157,33 @@ class TICED(nn.Module):
         layer1emb =  torch.sum(attn_weights1, 1).permute(1,0).unsqueeze(2).expand_as(embs_final) * embs_final
 
         beta1 = self.v_h2std(torch.sigmoid(output1 + layer1emb).view(-1, self.embedding_dim)).view(
-            mask.size()) # torch.Size([bs, 19])
-        #  beta1 = self.v_h2std(torch.sigmoid(ini_emb + layer1mask ).view(-1, self.embedding_dim)).view(mask.size())
+            mask.size()) 
 
         # item filter
         beta1 = self.sf(beta1)
-        beta1_v = torch.mean(beta1, 1, True)[0].expand_as(beta1) # torch.Size([bs, 19])
-        beta1_mask = beta1 - self.lambdas * beta1_v    #torch.Size([bs, 19])
+        beta1_v = torch.mean(beta1, 1, True)[0].expand_as(beta1) 
+        beta1_mask = beta1 - self.lambdas * beta1_v   
 
         beta1 = torch.where(beta1_mask > 0, beta1,
                            torch.tensor([0.], device=self.device))
 
         sess_std = torch.sum(beta1.unsqueeze(2).expand_as(embs_final) * embs_final, 1)
 
-        sess_std = self.dropout70(sess_std) # torch.Size([bs, 64])
+        sess_std = self.dropout70(sess_std) 
 
-        layer2cur = self.final2std2_cur(embs_final.contiguous().view(-1, self.embedding_dim)).view(embs_final.size()) #torch.Size([bs, 19, 64])
-        layer2_a = self.final2std2_std(sess_std) #torch.Size([bs, 64])
-        layer2_a_expand = layer2_a.unsqueeze(1).expand_as(layer2cur) #torch.Size([bs, 19, 64])
-        layer2mask = mask.unsqueeze(2).expand_as(layer2cur) * layer2_a_expand #torch.Size([bs, 19, 64])
+        layer2cur = self.final2std2_cur(embs_final.contiguous().view(-1, self.embedding_dim)).view(embs_final.size()) 
+        layer2_a = self.final2std2_std(sess_std) 
+        layer2_a_expand = layer2_a.unsqueeze(1).expand_as(layer2cur) 
+        layer2mask = mask.unsqueeze(2).expand_as(layer2cur) * layer2_a_expand 
 
 
         output2, attn_weights2 = self.multihead_attn(embs_final, layer2cur, layer2mask)
         layer2emb =  torch.sum(attn_weights2, 1).permute(1,0).unsqueeze(2).expand_as(embs_final) * embs_final
 
         beta2 = self.v_h2std(torch.sigmoid(output2 + layer2emb).view(-1, self.embedding_dim)).view(
-            mask.size()) # torch.Size([bs, 19])
+            mask.size())
 
-        beta2 = self.sf(beta2) #torch.Size([bs, 19])
+        beta2 = self.sf(beta2) 
         beta2_v = torch.mean(beta2, 1, True)[0].expand_as(beta2)
         beta2_mask = beta2 - self.lambdas * beta2_v
 
@@ -206,7 +202,7 @@ class TICED(nn.Module):
         layer3emb = torch.sum(attn_weights3, 1).permute(1,0).unsqueeze(2).expand_as(embs_final)* embs_final
 
         beta3 = self.v_h2std(torch.sigmoid(output3 + layer3emb).view(-1, self.embedding_dim)).view(
-            mask.size()) # torch.Size([bs, 19])
+            mask.size()) 
 
 
         beta3 = self.sf(beta3)
@@ -218,23 +214,22 @@ class TICED(nn.Module):
         sess_std3 = torch.sum(beta3.unsqueeze(2).expand_as(embs_final) * embs_final, 1) 
 
         sess_current = sess_std3 # torch.Size([bs, 64])
-
         
         # cosine similarity
         sess_norm = F.normalize(sess_current, p=2, dim=1)
-        sim_matrix = F.cosine_similarity(sess_norm.unsqueeze(1), sess_norm.unsqueeze(0), dim=2) #torch.Size([bs, bs])
+        sim_matrix = F.cosine_similarity(sess_norm.unsqueeze(1), sess_norm.unsqueeze(0), dim=2)
 
         k_v = self.neighbor_num
         if sim_matrix.size()[0] < k_v:
             k_v = sim_matrix.size()[0]
-        cos_topk, topk_indice = torch.topk(sim_matrix, k=k_v, dim=1) #torch.Size([bs, 5])
+        cos_topk, topk_indice = torch.topk(sim_matrix, k=k_v, dim=1)
         cos_topk = nn.Softmax(dim=-1)(cos_topk)
-        sess_topk = sess_current[topk_indice] # torch.Size([bs, 5, 64])
+        sess_topk = sess_current[topk_indice] 
 
-        cos_sim = cos_topk.unsqueeze(2).expand(cos_topk.size()[0], cos_topk.size()[1], self.embedding_dim) # torch.Size([bs, 5, 64])
+        cos_sim = cos_topk.unsqueeze(2).expand(cos_topk.size()[0], cos_topk.size()[1], self.embedding_dim)
 
         neighbor_sess = torch.sum(cos_sim * sess_topk, 1)
-        neighbor_sess = self.dropout40(neighbor_sess) #torch.Size([bs, 64])
+        neighbor_sess = self.dropout40(neighbor_sess) 
         sess_final = torch.cat(
             [sess_current, neighbor_sess, sess_current + neighbor_sess, sess_current * neighbor_sess], 1)
 
@@ -245,7 +240,7 @@ class TICED(nn.Module):
 
         item_embs = self.bn1(item_embs)
         scores = torch.matmul(sess_final, item_embs.permute(1, 0))
-        return scores  # torch.Size([bs, 43097])
+        return scores 
 
     def init_hidden(self, batch_size):
         return torch.zeros((self.n_layers, batch_size, self.hidden_size), requires_grad=True).to(self.device)
